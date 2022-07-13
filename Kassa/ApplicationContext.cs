72e628eba1cc119571099d38e7853254d62fc3e8
Владислав Берестюк <vladixerplay@gmail.com>
2022-07-13 16:24:45 +0300

    Добавьте файлы проекта.

---
+++ Kassa/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Kassa;
+
+namespace WpfApp2
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<FlowerStore> FlowerStore { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            ob.UseSqlite(@"Data Source=C:\Users\BeVL\source\repos\WpfApp2\WpfApp2\Flowers.db");
+        }
+    }
+}
