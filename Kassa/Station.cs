ffd8fd7d3f52cebabd70d43cbeaf8d686fd13126
Uamixerz <Uamixerz@gmail.com>
2022-07-15 15:24:37 +0300

    ф

---
+++ Kassa/Station.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Kassa
+{
+    public class Station
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+
+        public Station(string Name)
+        {
+            this.Name = Name;
+        }
+
+    }
+}
